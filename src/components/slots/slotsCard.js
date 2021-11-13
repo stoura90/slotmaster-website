c80c41dce17b6f5ac9b520277b72939293d9e0e7
zura.ph <zura.ph@gmail.com>
2021-11-13 23:04:59 +0400

    სლოტის გვერდზე სლოტ ქარდების ზომების შეცვლა

--- src/components/slots/slotsCard.js
+++ src/components/slots/slotsCard.js
@@ -11,19 +11,24 @@ const SlotCard =({data})=> {
 
     return (_.map(data, (v,index)=>{
                 return  (
-                    <div className="col-4 col-lg-3 col-xl-2" key={index}>
-                        <div className="slot-card">
+                    <div className="card-list-item" key={index} >
+                        <div className="sl-card" style={{backgroundImage:`url(${v.imageUrl})`}}/>
+                        <div className="info">
+                            <div className="name">{v.name}</div>
+                        </div>
+
+                        {/*<div className="slot-card" style={{backgroundImage:`url(${v.imageUrl})`}}>
                             {loader===v.gameId && <Loader/>}
-                            <div className="slot-card-hover animated-background" style={{backgroundImage:`url(${v.imageUrl})`}} onClick={()=>play(v)}>
+                            <div className="slot-card-hover animated-background"  onClick={()=>play(v)}>
                                 <div className="slot-card-cover "/>
-                                {/*<img src={v.imageUrl} alt="" style={{visibility:"hidden"}} />*/}
+                                <img src={v.imageUrl} alt="" style={{visibility:"hidden"}} />
                                 <img src={PlayIcon} alt="" className="play-btn" />
                             </div>
                             <div className="info">
                                 <div className="name">{v.name}</div>
-                                {/*<div className="like"><i/><span>{v?.options?.likes}</span></div>*/}
+                                <div className="like"><i/><span>{v?.options?.likes}</span></div>
                             </div>
-                        </div>
+                        </div>*/}
                     </div>
                 )
             })
@@ -31,3 +36,20 @@ const SlotCard =({data})=> {
     )
 }
 export default SlotCard;
+
+
+/*
+<div className="col-4 col-lg-3 col-xl-2" key={index}>
+    <div className="slot-card">
+        {loader===v.gameId && <Loader/>}
+        <div className="slot-card-hover animated-background" style={{backgroundImage:`url(${v.imageUrl})`}} onClick={()=>play(v)}>
+            <div className="slot-card-cover "/>
+            {/!*<img src={v.imageUrl} alt="" style={{visibility:"hidden"}} />*!/}
+            <img src={PlayIcon} alt="" className="play-btn" />
+        </div>
+        <div className="info">
+            <div className="name">{v.name}</div>
+            {/!*<div className="like"><i/><span>{v?.options?.likes}</span></div>*!/}
+        </div>
+    </div>
+</div>*/
